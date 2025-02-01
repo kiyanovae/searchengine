1a893d884822efd59a992da4626bbf02ccf81c24
Selogaz <Terentjev.selogaz@yandex.ru>
2025-02-01 10:39:18 +0300

    Created 2 entities and index

---
+++ src/main/java/searchengine/model/Page.java
@@ -0,0 +1,29 @@
+package searchengine.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity(name = "page")
+@Table(name = "page", indexes = { @Index(name = "path_index", columnList = "path") })
+public class Page {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "site_id", nullable = false)
+    private Site siteId;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String path;
+
+    @Column(nullable = false)
+    private Integer code;
+
+    @Column(columnDefinition = "MEDIUMTEXT",nullable = false)
+    private String content;
+}
---
+++ src/main/java/searchengine/model/Site.java
@@ -0,0 +1,37 @@
+package searchengine.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity(name = "site")
+@Table(name = "site")
+public class Site {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "ENUM('INDEXING', 'INDEXED', 'FAILED')", nullable = false)
+    private Status status;
+
+    @Column(name = "status_time", nullable = false)
+    private Date statusTime;
+
+    @Column(name = "last_error", columnDefinition = "TEXT")
+    private String lastError;
+
+    @Column(columnDefinition = "VARCHAR(255)", nullable = false)
+    private String url;
+
+    @Column(columnDefinition = "VARCHAR(255)", nullable = false)
+    private String name;
+
+
+
+}
